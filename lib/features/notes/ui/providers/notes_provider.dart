1fe519003a5d99e51caf520e7d6f4818688ce2c9
Moaz Talaat <88838071+MoazSalem@users.noreply.github.com>
2025-07-14 19:05:58 +0300

    Setup notesProvider with the new impls

--- lib/features/notes/ui/providers/notes_provider.dart
+++ lib/features/notes/ui/providers/notes_provider.dart
@@ -1,8 +1,39 @@
+import 'package:colorful_notes/features/notes/data/repository/note_repo_impl.dart';
 import 'package:colorful_notes/features/notes/domain/entities/note.dart';
+import 'package:colorful_notes/features/notes/domain/repositories/note_repository.dart';
+import 'package:colorful_notes/features/notes/domain/usecases/add_note.dart';
+import 'package:colorful_notes/features/notes/domain/usecases/delete_note.dart';
+import 'package:colorful_notes/features/notes/domain/usecases/get_notes.dart';
+import 'package:colorful_notes/features/notes/domain/usecases/update_note.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:colorful_notes/features/notes/ui/providers/database_provider.dart';
 
 final notesProvider = FutureProvider<List<Note>>((ref) async {
+  final useCase = ref.watch(getNotesUseCaseProvider);
+  return useCase();
+});
+
+final noteRepositoryProvider = Provider<NoteRepository>((ref) {
   final database = ref.watch(databaseProvider).value!;
-  return database.getAllNotes();
+  return NoteRepositoryImpl(database);
+});
+
+final getNotesUseCaseProvider = Provider<GetNotesUseCase>((ref) {
+  final repo = ref.watch(noteRepositoryProvider);
+  return GetNotesUseCase(repo);
+});
+
+final addNoteUseCaseProvider = Provider<AddNoteUseCase>((ref) {
+  final repo = ref.watch(noteRepositoryProvider);
+  return AddNoteUseCase(repo);
+});
+
+final updateNoteUseCaseProvider = Provider<UpdateNoteUseCase>((ref) {
+  final repo = ref.watch(noteRepositoryProvider);
+  return UpdateNoteUseCase(repo);
+});
+
+final deleteNoteUseCaseProvider = Provider<DeleteNoteUseCase>((ref) {
+  final repo = ref.watch(noteRepositoryProvider);
+  return DeleteNoteUseCase(repo);
 });
